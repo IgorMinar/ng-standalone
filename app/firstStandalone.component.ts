7bd1169841b5687249ae1a042d4a7d1775d47a46
IgorMinar <iminar@google.com>
2021-08-10 18:29:25 -0700

    4 files modified

--- app/firstStandalone.component.ts
+++ app/firstStandalone.component.ts
@@ -1,28 +1,15 @@
 import { Component } from '../standaloneShim';
 
-@XComponent({
+@Component({
   selector: 'first-standalone-component',
   standalone: true,
-  template: `I'm first! <button (click)="confirm()">click to confirm</button>`
+  template: `
+    I'm first!
+    <button (click)="confirm()">click to confirm</button>
+  `
 })
 export class FirstStandaloneComponent {
   confirm() {
     console.log('confirmed!');
   }
-}
-
-function XComponent(
-  componentMetadata: Component & { standalone: true }
-): ClassDecorator {
-  console.log(`XComponent:`, componentMetadata);
-
-  const compotentClazz = Component(componentMetadata);
-
-  @NgModule({
-    declarations: [[compotentClazz]],
-    imports: [CommonModule]
-  })
-  class VirtualNgModule {}
-
-  return compotentClazz;
 }
